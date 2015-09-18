e62bef4ebd8a7f058e075ea6808db0782eef5ec2
Edvin Hultberg <hultberg@mittnett.net>
2015-09-18 19:18:14 +0200

    Redisable debug mode

--- ppinnlevering/settings.py
+++ ppinnlevering/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '9=zucsu_us&87(35q$8@zzh5s_n^^wrx#6!3hg4b0myhtow#eq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ["innlevering.mittnett.net"]
 
