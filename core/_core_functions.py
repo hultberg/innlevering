73ad54251cc1e19d844bb92c5052e642162e600d
Edvin Hultberg <hultberg@mittnett.net>
2015-10-01 09:59:03 +0200

    Added method to check if user has voted
    Disable ge_has_valid_ticket

--- core/_core_functions.py
+++ core/_core_functions.py
@@ -1,5 +1,5 @@
 import requests
-from core.models import InnleveringUser
+from core.models import InnleveringUser, UserVote
 
 def user_has_group(user, groupName):
     # Determine if a user is in a group
@@ -13,15 +13,19 @@ def user_is_crew(user):
     return user_has_group(user, "Crew")
 
 
+def has_voted(user, bidrag):
+    vote = UserVote.objects.filter(user=user, bidrag=bidrag)
+
+    if vote.count() > 0:
+    	return True
+
+    return False
+
+
 def get_innlevering_user(user):
 	inn = InnleveringUser.objects.filter(user=user)
 	if inn.count() > 0:
 		return inn[0]
-
-
-# Determine if a user can vote on bidrags.
-def can_vote(user):
-	return user_is_crew(user) or ge_has_valid_ticket(user)
 
 
 # Determine if a user can upload a bidrag.
@@ -31,27 +35,27 @@ def can_upload(user):
 
 def ge_has_valid_ticket(user):
 	# Fetch the innlevering user of this one.
-	innleveringuser = get_innlevering_user(user)
+	#innleveringuser = get_innlevering_user(user)
 
-	if innleveringuser:	
-		data = {
-			"user_id": innleveringuser.geID,
-			"token": innleveringuser.currenttoken,
-			"timestamp": innleveringuser.currenttimestamp,
-			"eventID": 82
-		}
+	#if innleveringuser:	
+	#	data = {
+	#		"user_id": innleveringuser.geID,
+	#		"token": innleveringuser.currenttoken,
+	#		"timestamp": innleveringuser.currenttimestamp,
+	#		"eventID": 82
+	#	}
 
-		result = requests.post("https://www.geekevents.org/sso/user-has-tickets/", params=data)
+	#	result = requests.post("https://www.geekevents.org/sso/user-has-tickets/", params=data)
 
-		numberOfTickets = 0
+	#	numberOfTickets = 0
 
-		try:
-			numberOfTickets = int(result.text)
-		except ValueError:
-			return False
+	#	try:
+	#		numberOfTickets = int(result.text)
+	#	except ValueError:
+	#		return False
 
 		# Validate number of tickets
-		if numberOfTickets > 0:
-			return True
+	#	if numberOfTickets > 0:
+	#		return True
 
-	return False
+	return True
