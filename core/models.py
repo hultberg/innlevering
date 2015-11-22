67bd9256dfb8be8efdfe9f9b2686ae5e51117767
Edvin Hultberg <hultberg@mittnett.net>
2015-11-22 19:31:43 +0100

    License

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
+"""
+  Copyright 2015 Edvin Hultberg
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+"""
+
 from django.db import models
 import time
 import os
@@ -32,7 +48,7 @@ class Compo(models.Model):
     isPublished  = models.BooleanField(default=False)
     isVotingMode = models.BooleanField(default=False)
 
-    # 
+    #
     def __str__(self):
         return self.name
 
@@ -50,7 +66,7 @@ class Bidrag(models.Model):
     votes = models.IntegerField()
 
     class Meta:
-        unique_together = ('compo', 'creator',)	
+        unique_together = ('compo', 'creator',)
 
     #
     def __str__(self):
@@ -93,4 +109,4 @@ class UserVote(models.Model):
 
     #
     def __str__(self):
-        return self.user.username + " vote on bID: " + str(self.bidrag.id) 
+        return self.user.username + " vote on bID: " + str(self.bidrag.id)
